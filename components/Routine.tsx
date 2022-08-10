c4f7e0d86bb56296455aed48a484ef396ce2e5c3
Bibek Basyal <bibekbasyal22@gmail.com>
2022-08-10 20:50:11 +0545

    data not found and holiday are now more graphical

--- components/Routine.tsx
+++ components/Routine.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import Class from "./Class";
 import Loading from "./Loading";
@@ -19,6 +20,17 @@ const Routine: React.FC<IRoutine> = ({ data, isLoading }) => {
       return (
         <div className="max-w-5xl margin-auto">
           <div className="flex flex-col space-y-8">
+            {data?.classRoutine.length === 0 && (
+              <div className="flex justify-center items-center flex-col space-y-6 text-slate-500">
+                <Image
+                  src={"/hurray.svg"}
+                  width={200}
+                  height={200}
+                  alt="Holiday"
+                />
+                <p>It seems like there is holiday.</p>
+              </div>
+            )}
             {data?.classRoutine?.map((classData: any) => (
               <Class key={classData.id} classData={classData} />
             ))}
@@ -28,7 +40,10 @@ const Routine: React.FC<IRoutine> = ({ data, isLoading }) => {
     } else {
       return (
         <div className="max-w-5xl margin-auto">
-          Oops, no published Data found. Contact CR.
+          <div className="flex justify-center items-center flex-col space-y-6 text-slate-500">
+            <Image src={"/nodata.svg"} width={200} height={200} alt="No Data" />
+            <p>Oops, seems like your CR has not posted the routine yet.</p>
+          </div>
         </div>
       );
     }
